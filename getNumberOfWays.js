f2faef809d71010b6dff06d2ef1de2e7383d5628
scripturesdev <scripturesdev@gmail.com>
2023-06-19 22:09:25 +0200

    .

---
+++ getNumberOfWays.js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/647d08a2c736e3777c9ae1db/train/javascript
+const getNumberOfWays = (steps, maxJumpLength) => {
+  
+  return 0
+}
+
+/*
+steps =3
+max = 3
+111
+12
+21
+3
+
+. first way is 111..steps
+. increment first item -> create new way if
+*/
